1b941f3a30797d014b8694ac8d8ac6f723cba752
anaele nnaemeka <anaelennaemeka@gmail.com>
2023-01-20 14:40:29 +0100

    implemented named Routes

--- src/router/index.js
+++ src/router/index.js
@@ -32,13 +32,10 @@ const routes = [
       import(/* webpackChunkName: "panama"  */ "../views/PanamaView.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/destination/:id",
+    name: "destination.show",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/*  webpackChunkName: "panama" */ "../views/DestinationView.vue"),
   },
 ];
 
