73eaf5490e3f263e79f9e4d3e730b16c945a65b6
anaele nnaemeka <anaelennaemeka@gmail.com>
2023-01-23 19:19:42 +0100

    Route Guards

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>Not Found</h1>
-  <p>
-    Oops, We couldn't find that page
-    <router-link to="/">home</router-link>
-  </p>
+  <div>
+    <h1>Not Found</h1>
+    <p>
+      Oops, We couldn't find that page
+      <router-link to="/">home</router-link>
+    </p>
+  </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import sourceData from "@/data.json";
 
 const routes = [
   {
@@ -13,6 +14,19 @@ const routes = [
     component: () =>
       import(/*  webpackChunkName: "panama" */ "../views/DestinationView.vue"),
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to) {
+      const exists = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      );
+      if (!exists) {
+        return {
+          name: "NotFound",
+          params: { pathMatch: to.path.split("/").slice(1) },
+          query: to.query,
+          hash: to.hash,
+        };
+      }
+    },
     children: [
       {
         path: ":experienceSlug",
